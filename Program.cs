0f25a63f397cab5328ddc8a5e78010835442c6a4
gya1233 <1514215288@qq.com>
2025-02-26 19:09:30 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入两个数字：");
+            Console.WriteLine("");
+
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+            Console.WriteLine("请输入运算符：");
+            var c = Console.ReadLine();
+            if (c == "+")
+            { Console.WriteLine(a+ "+"+ b+ "="+ (int.Parse(a) + int.Parse(b))); }
+            else if (c == "-"){ 
+                Console.WriteLine(a + "-" + b + "=" 
+                    + (int.Parse(a) - int.Parse(b))); }
+            
+               else if (c == "*")
+                {
+                    Console.WriteLine(a + "*" + b + "="
+                        + (int.Parse(a) * int.Parse(b)));
+                }
+
+            else if (c == "/")
+            {
+                Console.WriteLine(a + "/" + b + "="
+                    + (int.Parse(a) / int.Parse(b)));
+            }
+        }
+    }
+}

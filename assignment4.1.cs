a9ede913c130d51aac1e90966882112ecd49827c
gya1233 <1514215288@qq.com>
2025-03-19 23:46:53 +0800

    Add files via upload

---
+++ assignment4.1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public class LinkedList<T>
+{
+    public class Node
+    {
+        public T Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public Node Head { get; private set; }
+
+    public void Add(T data)
+    {
+        Node newNode = new Node(data);
+        if (Head == null)
+        {
+            Head = newNode;
+        }
+        else
+        {
+            Node current = Head;
+            while (current.Next != null)
+                current = current.Next;
+            current.Next = newNode;
+        }
+    }
+}
+
+public static class LinkedListExtensions
+{
+    public static void ForEach<T>(this LinkedList<T> list, Action<T> action)
+    {
+        LinkedList<T>.Node current = list.Head;
+        while (current != null)
+        {
+            action(current.Data);
+            current = current.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.Add(10);
+        list.Add(20);
+        list.Add(30);
+
+        // 使用扩展方法打印元素
+        Console.WriteLine("Elements:");
+        list.ForEach(x => Console.WriteLine(x));
+
+        // 其他操作同上...
+    }
+}
---
+++ assignment4.2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Timers;
+
+public class AlarmClock2
+{
+    private Timer timer;
+
+    public event EventHandler Tick;
+    public event EventHandler Alarm;
+
+    public DateTime AlarmTime { get; set; }
+
+    public void Start()
+    {
+        timer = new Timer(1000);
+        timer.Elapsed += (sender, e) =>
+        {
+            Tick?.Invoke(this, EventArgs.Empty);
+            if (DateTime.Now >= AlarmTime)
+            {
+                Alarm?.Invoke(this, EventArgs.Empty);
+                timer.Stop();
+            }
+        };
+        timer.AutoReset = true;
+        timer.Start();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var clock = new AlarmClock2();
+        clock.AlarmTime = DateTime.Now.AddSeconds(5);
+        clock.Tick += (s, e) => Console.WriteLine($"[Tick] {DateTime.Now.ToString("HH:mm:ss")}");
+        clock.Alarm += (s, e) => Console.WriteLine($"[Alarm] Time's up at {DateTime.Now.ToString("HH:mm:ss")}!");
+        clock.Start();
+        Console.ReadLine();
+    }
+}
